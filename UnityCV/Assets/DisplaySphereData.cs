9d5a664b13c4479d0f21d098ee4fb3110a1fc89c
james-massey <james.a.massey@ndsu.edu>
2015-04-22 09:32:31 -0500

    Added Unity Project
    Unity Project to create application.

---
+++ UnityCV/Assets/DisplaySphereData.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DisplaySphereData : MonoBehaviour 
+{
+	private float x_coord=0.0f;
+	private float y_coord=0.0f;
+	private float z_coord=0.0f;
+
+	public Text x_text;
+	public Text y_text;
+	public Text z_text;
+
+	public Transform sphere_Transform;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (sphere_Transform) 
+		{
+			x_coord = sphere_Transform.transform.position.x;
+			y_coord = sphere_Transform.transform.position.y;
+			z_coord = sphere_Transform.transform.position.z;
+
+			x_text.text = x_coord.ToString ();
+			y_text.text = y_coord.ToString ();
+			z_text.text = z_coord.ToString ();
+		}
+	}
+}
